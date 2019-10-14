82175e50786db4a57446e989423d57ac02fe5423
AOE Takashi <aoe1219@gmail.com>
2019-10-14 19:59:47 +0900

    Add file reader and writer for file encoding

---
+++ src/main/java/ninja/javafx/smartcsv/preferences/CharsetHelper.java
@@ -0,0 +1,18 @@
+package ninja.javafx.smartcsv.preferences;
+
+import java.nio.charset.Charset;
+
+/**
+ * Helper class for getting charset's name
+ */
+public class CharsetHelper {
+
+    public static String getCharsetName(String name) {
+        try {
+            return Charset.forName(name).name();
+        } catch (Exception e) {
+            return Charset.defaultCharset().name();
+        }
+    }
+
+}
---
+++ src/main/java/ninja/javafx/smartcsv/preferences/EncodingFileWriter.java
@@ -0,0 +1,33 @@
+package ninja.javafx.smartcsv.preferences;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ninja.javafx.smartcsv.FileWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Save the file encoding to configuration file
+ */
+public class EncodingFileWriter implements FileWriter<String> {
+
+    private String fileEncoding;
+
+    @Override
+    public void setContent(String fileEncoding) {
+        this.fileEncoding = fileEncoding;
+    }
+
+    @Override
+    public void write(File filename) throws IOException {
+        Map<String, String> encodings = new HashMap<>();
+        encodings.put("fileEncoding", CharsetHelper.getCharsetName(fileEncoding));
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Files.write(filename.toPath(), gson.toJson(encodings).getBytes());
+    }
+
+}
